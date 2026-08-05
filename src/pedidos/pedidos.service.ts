8ecb22ee2aa55ddc391f431437aceb5a7b967a62
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2026-08-05 09:21:32 -0300

    Delete pedido + Melhoria na criação

--- src/pedidos/pedidos.service.ts
+++ src/pedidos/pedidos.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { Pedido } from './entities/pedido.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, QueryFailedError, Repository } from 'typeorm';
 import { Vendedor } from 'src/vendedores/entities/vendedor.entity';
 
 @Injectable()
@@ -17,28 +21,34 @@ export class PedidosService {
   ) {}
 
   async create(createPedidoDto: CreatePedidoDto) {
-    console.log(createPedidoDto);
-    const vendedor = await this.vendedoresRepository.findOneBy({
-      id: createPedidoDto.vendedorId,
-    });
+    try {
+      const vendedor = await this.vendedoresRepository.findOneBy({
+        id: createPedidoDto.vendedorId,
+      });
 
-    if (!vendedor) {
-      throw new NotFoundException('Vendedor não encontrado.');
+      if (!vendedor) {
+        throw new NotFoundException('Vendedor não encontrado.');
+      }
+
+      const pedido = this.pedidosRepository.create({
+        numero: createPedidoDto.numero,
+        data: new Date(createPedidoDto.data),
+        pesoKg: createPedidoDto.pesoKg,
+        valor: createPedidoDto.valor,
+        vendedor,
+      });
+
+      return await this.pedidosRepository.save(pedido);
+    } catch (error) {
+      if (
+        error instanceof QueryFailedError &&
+        (error as any).driverError?.code === '23505' // PostgreSQL: unique_violation
+      ) {
+        throw new BadRequestException('Já existe um pedido com esse número.');
+      }
+
+      throw error;
     }
-
-    console.log(vendedor);
-
-    const pedido = this.pedidosRepository.create({
-      numero: createPedidoDto.numero,
-      data: new Date(createPedidoDto.data),
-      pesoKg: createPedidoDto.pesoKg,
-      valor: createPedidoDto.valor,
-      vendedor,
-    });
-
-    console.log(pedido);
-
-    return this.pedidosRepository.save(pedido);
   }
 
   async findAll(): Promise<Pedido[]> {
@@ -68,7 +78,7 @@ export class PedidosService {
     return `update`;
   }
 
-  remove(id: number) {
-    return `remove`;
+  async remove(id: number): Promise<DeleteResult> {
+    return await this.pedidosRepository.delete(id);
   }
 }
